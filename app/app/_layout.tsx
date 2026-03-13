a3ed4bfec2be95b6a701bf55acb3cec274a52968
Harvard Chong <harvardchong1@gmail.com>
2026-03-13 19:58:38 +1100

    Set up Hoerui

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { HeroUINativeProvider } from "heroui-native";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Stack />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
 }
