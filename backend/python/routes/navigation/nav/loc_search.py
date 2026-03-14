27bb62e74f425a4297a5ee8f3eecf5ef2d5a9d81
Ken Zhet Saw <ksaw0100@student.monash.edu>
2026-03-14 17:28:44 +1100

    Added variables for slime location and slime bond level, Also added a random location picker on the data's results.

--- backend/python/routes/navigation/nav/loc_search.py
+++ backend/python/routes/navigation/nav/loc_search.py
@@ -1,11 +1,16 @@
 import requests
+import random
 
 endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 
-locationcoord = "-33.8670522,151.1957362"
-radius = 1500 # Placeholder
-keyword = "restaurant"
-API_Key = "AIzaSyBkCPN6Fa7qDphn2oteJrY-wXC66_4arpM" #placeholder
+#put post endpoint here
+slimeloc = "-33.8670522,151.1957362" #placeholder, user data is supposed to be here
+slimebond =  2 #placeholder , user data is supposed to be here
+
+locationcoord = slimeloc
+radius = 500 *slimebond 
+keyword = "restaurant" #placeholder
+API_Key = "AIzaSyBZ38KmbsMg2jNhKywyw8Yo84sWw8xrmm4" 
 
 params = {
     "location": locationcoord,
@@ -16,7 +21,11 @@ params = {
 
 response = requests.get(endpoint_url, params=params)
 data = response.json()
-print(data)
+
 
 #for location in data["results"]:
     #print(f"name: {location["name"]}, location :{location["geometry"]["location"]}")
+#len(data["results"])
+
+randlocation = data["results"][random.randint(0, int(len(data["results"])) - 1)]
+print(f"name: {randlocation["name"]}, location :{randlocation["geometry"]["location"]}")
